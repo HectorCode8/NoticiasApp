ee09b5fee39a5b6d482a6e48a1e26789cbdbda72
Hector Haro H <hector8.haro@gmail.com>
2022-05-11 16:30:57 -0500

    Consulting news from de API

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,10 +1,22 @@
-import { useState, useEffcet, createContext } from 'react'
+import axios from 'axios'
+import { useState, useEffect, createContext } from 'react'
 
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children}) => {
     const [categoria, setCategoria] = useState('general')
+    const [noticias, setNoticias] = useState([])
+
+    useEffect(() => {
+        const consultarApi = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&pageSize=100&apiKey=${import.meta.env.VITE_API_KEY}`
+            const {data} = await axios(url)
+
+            setNoticias(data.articles)
+        }
+        consultarApi()
+    }, [categoria])
 
     const handleChangeCategoria = e => {
         setCategoria(e.target.value)
